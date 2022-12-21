8030737368c7faece0e1af4629985ff8e8285b1f
Dom Mistry <dominicmistry1@gmail.com>
2022-12-21 17:45:58 +0000

    Recipe Card looks much better

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React, {useState} from 'react';
-import { Typography, Link, AppBar, Box, Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Fab, Stack, Autocomplete, TextField} from '@mui/material';
+import { styled } from '@mui/material/styles';
+import { Typography, Link, AppBar, Box, Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Fab, Stack, Autocomplete, TextField, CardHeader} from '@mui/material';
+import Avatar from '@mui/material/Avatar';
 import MenuBookIcon from '@mui/icons-material/MenuBook';
 import AddIcon from '@mui/icons-material/Add';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import ThumbDownIcon from '@mui/icons-material/ThumbDown';
+import { green } from '@mui/material/colors';
+import IconButton from '@mui/material/IconButton';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Collapse from '@mui/material/Collapse';
 
 
 const cards = [1,2,3,4,5,6,7,8,9];
@@ -19,10 +27,27 @@ const tags = [
 
 ]
 
+const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+  })(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  }));
+
 const App = () => {
 
     const [countUp, setCountUp] = useState(0)
     const [countDown, setCountDown] = useState(0)
+
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleExpandClick = () => {
+      setExpanded(!expanded);
+    };
 
     return <>
         <CssBaseline />
@@ -79,30 +104,78 @@ const App = () => {
                     {cards.map((card) => (
                         <Grid item key={card} xs={12} sm={6} md={4}>
                         <Card sx={{height: '100%', display: 'flex', flexDirection: 'column'}}>
+                            <CardHeader
+                                avatar={
+                                        <Avatar sx={{ bgcolor: green[500] }} aria-label="recipe">
+                                        <MenuBookIcon/>
+                                        </Avatar>
+                                    }
+                                    title="Recipe Title"
+                                    subheader="December 21, 2022">
+
+                            </CardHeader>
                             <CardMedia sx={{paddingTop: '56.25%' }} //16:9
                             image="https://source.unsplash.com/random"
                             title="Image title"
                             />
                             <CardContent sx= {{flexGrow: 1}}>
-                                <Typography gutterBottom variant='h5'>
-                                    Recipe Title
-                                </Typography>
                                 <Typography>
-                                    This is a recipe card. You can use this section to describe the recipe. 
+                                    This is a recipe card. You can use this section to big up the recipe and tempt people to click it. 
                                 </Typography>
                             </CardContent>   
-                            <CardActions>
-                                <Button size="small" color="primary">View</Button>
+                            <CardActions disableSpacing>
+                                <IconButton aria-label="add to favorites">
+                                    <FavoriteIcon />
+                                </IconButton>
+                                <IconButton aria-label="share">
+                                    <ShareIcon />
+                                </IconButton> 
                                 <Button onClick={() => setCountUp(countUp + 1)}>
                                     <ThumbUpIcon />
-                                    {`${countUp === 0 ? '' : countUp}`} 
+                                    {`${countUp === 0 ? ' ' : countUp}`} 
                                 </Button>
                                 <Button onClick={() => setCountDown(countDown +1)}>
                                     <ThumbDownIcon />
                                     {`${countDown === 0 ? ' ' : countDown}`}
-                                </Button>
-                                
-                            </CardActions>                         
+                                </Button>  
+                                <ExpandMore
+                                        expand={expanded}
+                                        onClick={handleExpandClick}
+                                        aria-expanded={expanded}
+                                        aria-label="show more"
+                                        >
+                                    <ExpandMoreIcon />
+                                </ExpandMore>           
+                            </CardActions>
+                            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                            <CardContent>
+                            <Typography paragraph>Method:</Typography>
+                            <Typography paragraph>
+                                Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
+                                aside for 10 minutes.
+                            </Typography>
+                            <Typography paragraph>
+                                Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
+                                medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
+                                occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
+                                large plate and set aside, leaving chicken and chorizo in the pan. Add
+                                pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
+                                stirring often until thickened and fragrant, about 10 minutes. Add
+                                saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+                            </Typography>
+                            <Typography paragraph>
+                                Add rice and stir very gently to distribute. Top with artichokes and
+                                peppers, and cook without stirring, until most of the liquid is absorbed,
+                                15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
+                                mussels, tucking them down into the rice, and cook again without
+                                stirring, until mussels have opened and rice is just tender, 5 to 7
+                                minutes more. (Discard any mussels that don&apos;t open.)
+                            </Typography>
+                            <Typography>
+                                Set aside off of the heat to let rest for 10 minutes, and then serve.
+                            </Typography>
+                            </CardContent>
+                        </Collapse>                         
                         </Card>
                     </Grid>
                     ))} 
