acd564fa855ff4ec3ae9b1046d9ac1c1569b1b6a
Dom Mistry <dominicmistry1@gmail.com>
2022-12-22 22:24:10 +0000

    menu added with edit and delete options

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import SvgIcon from '@mui/material/SvgIcon';
-import { styled } from '@mui/material/styles';
+import { styled, alpha  } from '@mui/material/styles';
 import { Typography, Link, AppBar, Box, Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Fab, Autocomplete, TextField, CardHeader, Tabs, Tab, Tooltip} from '@mui/material';
 import Avatar from '@mui/material/Avatar';
 import MenuBookIcon from '@mui/icons-material/MenuBook';
@@ -18,7 +18,53 @@ import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 import LocalOfferSharpIcon from '@mui/icons-material/LocalOfferSharp';
 import SearchSharpIcon from '@mui/icons-material/SearchSharp';
 import ClickAwayListener from '@mui/material/ClickAwayListener';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import EditIcon from '@mui/icons-material/Edit';
+import Divider from '@mui/material/Divider';
+import DeleteSharpIcon from '@mui/icons-material/DeleteSharp';
 
+const StyledMenu = styled((props) => (
+    <Menu
+      elevation={0}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'right',
+      }}
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      {...props}
+    />
+  ))(({ theme }) => ({
+    '& .MuiPaper-root': {
+      borderRadius: 6,
+      marginTop: theme.spacing(1),
+      minWidth: 180,
+      color:
+        theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
+      boxShadow:
+        'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+      '& .MuiMenu-list': {
+        padding: '4px 0',
+      },
+      '& .MuiMenuItem-root': {
+        '& .MuiSvgIcon-root': {
+          fontSize: 18,
+          color: theme.palette.text.secondary,
+          marginRight: theme.spacing(1.5),
+        },
+        '&:active': {
+          backgroundColor: alpha(
+            theme.palette.primary.main,
+            theme.palette.action.selectedOpacity,
+          ),
+        },
+      },
+    },
+  }));
 
 const cards = [1,2,3,4,5,6,7,8,9];
 
@@ -89,6 +135,15 @@ const ExpandMore = styled((props) => {
   }
 
 const App = () => {
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open2 = Boolean(anchorEl);
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
 
     let author ='Uploader: DM1st';
     
@@ -227,7 +282,42 @@ const App = () => {
                                         </Avatar>
                                     }
                                     title="Recipe Title"
-                                    subheader= {author} />
+                                    subheader= {author} 
+                                    action={
+                                        <div>
+                                            <IconButton 
+                                            id="customized-hamburger-button"
+                                            aria-controls={open2 ? 'customized-hamburger-menu' : undefined}
+                                            aria-label="settings" 
+                                            onClick={handleClick} 
+                                            aria-haspopup="true"
+                                            aria-expanded={open2 ? 'true' : undefined}
+                                            variant="contained" 
+                                            >
+                                                <MoreVertIcon />
+                                            </IconButton>
+                                                <StyledMenu
+                                                    id="customized-hamburger-menu"
+                                                    MenuListProps={{
+                                                    'aria-labelledby': 'customized-hamburger-button',
+                                                    }}
+                                                    anchorEl={anchorEl}
+                                                    open={open2}
+                                                    onClose={handleClose}
+                                                >
+                                                    <MenuItem onClick={handleClose} disableRipple>
+                                                        <EditIcon />
+                                                        Edit
+                                                    </MenuItem>
+                                                    <Divider sx={{ my: 0.5 }} />
+                                                    <MenuItem onClick={handleClose} disableRipple>
+                                                        <DeleteSharpIcon />
+                                                        Delete
+                                                    </MenuItem>
+                                                </StyledMenu>  
+                                        </div>
+                                      }                                   
+                            />
                             <CardMedia sx={{paddingTop: '56.25%' }} //16:9
                             image="https://source.unsplash.com/ykThMylLsbY"
                             title="Image title"
