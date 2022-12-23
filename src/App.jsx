fda0b631fc52331f7fc0cdce205a7e5e48d94166
Dom Mistry <dominicmistry1@gmail.com>
2022-12-23 23:57:10 +0000

    fix typo on add dialog

--- src/App.jsx
+++ src/App.jsx
@@ -273,7 +273,7 @@ const App = () => {
         return(
 
           <Container sx={{width:'100%', display:'flex', align:'center', paddingTop:'5px', paddingBottom:'5px', justifyContent:'space-between', paddingRight:'0', paddingLeft:'0'}}>
-                <Typography color="textSecondary">Add Recipe method</Typography>
+                <Typography color="textSecondary">Add recipe method</Typography>
                 <Button variant="outlined" >
                   Method
                 </Button>
