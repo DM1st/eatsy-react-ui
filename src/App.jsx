70605699a1baba65c1be8b884c2caf7a39d51c28
Dom Mistry <dominicmistry1@gmail.com>
2022-12-30 22:27:11 +0000

    split out RecipeCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import {React, useState } from 'react';
 
 import RecipeDialog from './RecipeDialog';
 import FilterByTags from './FilterByTags';
@@ -6,69 +6,57 @@ import FilterByTags from './FilterByTags';
 import PropTypes from 'prop-types';
 import SvgIcon from '@mui/material/SvgIcon';
 import { styled, alpha } from '@mui/material/styles';
-import { Typography, Link, AppBar, Box, Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Fab, Autocomplete, TextField, CardHeader, Tabs, Tab, Tooltip } from '@mui/material';
-import Avatar from '@mui/material/Avatar';
+import { Typography, Link, AppBar, Box, CssBaseline, Grid, Toolbar, Container, Fab, Autocomplete, TextField, Tabs, Tab, Tooltip } from '@mui/material';
 import MenuBookIcon from '@mui/icons-material/MenuBook';
 import AddIcon from '@mui/icons-material/Add';
-import ThumbUpIcon from '@mui/icons-material/ThumbUp';
-import ThumbDownIcon from '@mui/icons-material/ThumbDown';
-import { green } from '@mui/material/colors';
-import IconButton from '@mui/material/IconButton';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import Collapse from '@mui/material/Collapse';
-import LocalPizzaSharpIcon from '@mui/icons-material/LocalPizzaSharp';
+
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 import LocalOfferSharpIcon from '@mui/icons-material/LocalOfferSharp';
 import SearchSharpIcon from '@mui/icons-material/SearchSharp';
 import ClickAwayListener from '@mui/material/ClickAwayListener';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import EditIcon from '@mui/icons-material/Edit';
-import Divider from '@mui/material/Divider';
-import DeleteSharpIcon from '@mui/icons-material/DeleteSharp';
 
-const StyledMenu = styled((props) => (
-  <Menu
-    elevation={0}
-    anchorOrigin={{
-      vertical: 'bottom',
-      horizontal: 'right',
-    }}
-    transformOrigin={{
-      vertical: 'top',
-      horizontal: 'right',
-    }}
-    {...props}
-  />
-))(({ theme }) => ({
-  '& .MuiPaper-root': {
-    borderRadius: 6,
-    marginTop: theme.spacing(1),
-    minWidth: 180,
-    color:
-      theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
-    boxShadow:
-      'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
-    '& .MuiMenu-list': {
-      padding: '4px 0',
-    },
-    '& .MuiMenuItem-root': {
-      '& .MuiSvgIcon-root': {
-        fontSize: 18,
-        color: theme.palette.text.secondary,
-        marginRight: theme.spacing(1.5),
-      },
-      '&:active': {
-        backgroundColor: alpha(
-          theme.palette.primary.main,
-          theme.palette.action.selectedOpacity,
-        ),
-      },
-    },
-  },
-}));
+import RecipeCard from './RecipeCard';
+
+// const StyledMenu = styled((props) => (
+//   <Menu
+//     elevation={0}
+//     anchorOrigin={{
+//       vertical: 'bottom',
+//       horizontal: 'right',
+//     }}
+//     transformOrigin={{
+//       vertical: 'top',
+//       horizontal: 'right',
+//     }}
+//     {...props}
+//   />
+// ))(({ theme }) => ({
+//   '& .MuiPaper-root': {
+//     borderRadius: 6,
+//     marginTop: theme.spacing(1),
+//     minWidth: 180,
+//     color:
+//       theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
+//     boxShadow:
+//       'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+//     '& .MuiMenu-list': {
+//       padding: '4px 0',
+//     },
+//     '& .MuiMenuItem-root': {
+//       '& .MuiSvgIcon-root': {
+//         fontSize: 18,
+//         color: theme.palette.text.secondary,
+//         marginRight: theme.spacing(1.5),
+//       },
+//       '&:active': {
+//         backgroundColor: alpha(
+//           theme.palette.primary.main,
+//           theme.palette.action.selectedOpacity,
+//         ),
+//       },
+//     },
+//   },
+// }));
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
@@ -83,16 +71,6 @@ const recipes = [
 
 ]
 
-const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -129,25 +107,13 @@ function a11yProps(index) {
 
 function App() {
 
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open2 = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
 
-  let author = 'Uploader: DM1st';
 
-  const [countUp, setCountUp] = useState(5)
-  const [countDown, setCountDown] = useState(2)
+  
 
-  const [expanded, setExpanded] = React.useState(false);
 
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
+
+
 
   const [value, setValue] = React.useState(0);
 
@@ -285,110 +251,7 @@ function App() {
           <Grid container spacing={4}>
             {cards.map((card) => (
               <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
-                  <CardHeader
-                    avatar={
-                      <Avatar sx={{ bgcolor: green[500] }} aria-label="recipe">
-                        <LocalPizzaSharpIcon />
-                      </Avatar>
-                    }
-                    title="Recipe Title"
-                    subheader={author}
-                    action={
-                      <div>
-                        <IconButton
-                          id="customized-hamburger-button"
-                          aria-controls={open2 ? 'customized-hamburger-menu' : undefined}
-                          aria-label="settings"
-                          onClick={handleClick}
-                          aria-haspopup="true"
-                          aria-expanded={open2 ? 'true' : undefined}
-                          variant="contained"
-                        >
-                          <MoreVertIcon />
-                        </IconButton>
-                        <StyledMenu
-                          id="customized-hamburger-menu"
-                          MenuListProps={{
-                            'aria-labelledby': 'customized-hamburger-button',
-                          }}
-                          anchorEl={anchorEl}
-                          open={open2}
-                          onClose={handleClose}
-                        >
-                          <MenuItem onClick={handleClose} disableRipple>
-                            <EditIcon />
-                            Edit
-                          </MenuItem>
-                          <Divider sx={{ my: 0.5 }} />
-                          <MenuItem onClick={handleClose} disableRipple disabled='true'>
-                            <DeleteSharpIcon />
-                            Delete
-                          </MenuItem>
-                        </StyledMenu>
-                      </div>
-                    }
-                  />
-                  <CardMedia sx={{ paddingTop: '56.25%' }} //16:9
-                    image="https://source.unsplash.com/ykThMylLsbY"
-                    title="Image title"
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography>
-                      This is a recipe card. You can use this section to big up the recipe and tempt people to click it.
-                    </Typography>
-                  </CardContent>
-                  <CardActions disableSpacing>
-                    <IconButton aria-label="share">
-                      <ShareIcon />
-                    </IconButton>
-                    <Button onClick={() => setCountUp(countUp + 1)}>
-                      <ThumbUpIcon />
-                      {`${countUp === 0 ? ' ' : countUp}`}
-                    </Button>
-                    <Button onClick={() => setCountDown(countDown + 1)}>
-                      <ThumbDownIcon />
-                      {`${countDown === 0 ? ' ' : countDown}`}
-                    </Button>
-                    <ExpandMore
-                      expand={expanded}
-                      onClick={handleExpandClick}
-                      aria-expanded={expanded}
-                      aria-label="show more"
-                    >
-                      <ExpandMoreIcon />
-                    </ExpandMore>
-                  </CardActions>
-                  <Collapse in={expanded} timeout="auto" unmountOnExit>
-                    <CardContent>
-                      <Typography paragraph>Method:</Typography>
-                      <Typography paragraph>
-                        Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-                        aside for 10 minutes.
-                      </Typography>
-                      <Typography paragraph>
-                        Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-                        medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-                        occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-                        large plate and set aside, leaving chicken and chorizo in the pan. Add
-                        pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-                        stirring often until thickened and fragrant, about 10 minutes. Add
-                        saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-                      </Typography>
-                      <Typography paragraph>
-                        Add rice and stir very gently to distribute. Top with artichokes and
-                        peppers, and cook without stirring, until most of the liquid is absorbed,
-                        15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-                        mussels, tucking them down into the rice, and cook again without
-                        stirring, until mussels have opened and rice is just tender, 5 to 7
-                        minutes more. (Discard any mussels that don&apos;t open.)
-                      </Typography>
-                      <Typography>
-                        Set aside off of the heat to let rest for 10 minutes, and then serve.
-                      </Typography>
-                    </CardContent>
-                  </Collapse>
-                </Card>
+                <RecipeCard />
               </Grid>
             ))}
           </Grid>
