0f154acde3a09e5b75225329d06322c1bbf329ee
Dom Mistry <dominicmistry1@gmail.com>
2022-12-31 00:24:44 +0000

    fixed errors from splitting out into individual functions

--- src/EatsyAppBar.jsx
+++ src/EatsyAppBar.jsx
@@ -1,4 +1,4 @@
-import {React, useState } from 'react';
+import { React, useState } from 'react';
 
 import { Typography, AppBar, Grid, Toolbar, Tooltip } from '@mui/material';
 import ClickAwayListener from '@mui/material/ClickAwayListener';
@@ -8,47 +8,50 @@ import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 
 function EatsyAppBar() {
 
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    const handleTooltipClose = () => {
-      setOpen(false);
-    };
-  
-    const handleTooltipOpen = () => {
-      setOpen(true);
-    };
+  const handleTooltipClose = () => {
+    setOpen(false);
+  };
+
+  const handleTooltipOpen = () => {
+    setOpen(true);
+  };
+
+  return (
 
     <AppBar position="relative">
-        <Grid container direction='row' alignItems='center' justifyContent='space-between'>
-          <Toolbar>
-            <MenuBookIcon sx={{ marginRight: '15px' }} />
-          </Toolbar>
-          <Typography variant="h6">
-            Eatsy Recipe App
-          </Typography>
-          <Toolbar sx={{ paddingTop: '4px', marginLeft: '0px' }}>
-            <ClickAwayListener onClickAway={handleTooltipClose}>
-              <div>
-                <Tooltip
-                  PopperProps={{
-                    disablePortal: true,
-                  }}
-                  onClose={handleTooltipClose}
-                  open={open}
-                  disableFocusListener
-                  disableHoverListener
-                  disableTouchListener
-                  title="Eatsy is an online recipe book to allow users to find, create and share their favourite recipes with other cooking and baking enthusiasts!"
-                >
-                  <iconButton onClick={handleTooltipOpen}>
-                    <InfoOutlinedIcon />
-                  </iconButton>
-                </Tooltip>
-              </div>
-            </ClickAwayListener>
-          </Toolbar>
-        </Grid>
-      </AppBar>
+      <Grid container direction='row' alignItems='center' justifyContent='space-between'>
+        <Toolbar>
+          <MenuBookIcon sx={{ marginRight: '15px' }} />
+        </Toolbar>
+        <Typography variant="h6">
+          Eatsy Recipe App
+        </Typography>
+        <Toolbar sx={{ paddingTop: '4px', marginLeft: '0px' }}>
+          <ClickAwayListener onClickAway={handleTooltipClose}>
+            <div>
+              <Tooltip
+                PopperProps={{
+                  disablePortal: true,
+                }}
+                onClose={handleTooltipClose}
+                open={open}
+                disableFocusListener
+                disableHoverListener
+                disableTouchListener
+                title="Eatsy is an online recipe book to allow users to find, create and share their favourite recipes with other cooking and baking enthusiasts!"
+              >
+                <iconButton onClick={handleTooltipOpen}>
+                  <InfoOutlinedIcon />
+                </iconButton>
+              </Tooltip>
+            </div>
+          </ClickAwayListener>
+        </Toolbar>
+      </Grid>
+    </AppBar>
+  )
 
 }
 
