24b9443e22db729a7c5cda79f1a87637051dbdf9
Dom Mistry <dominicmistry1@gmail.com>
2023-01-01 22:26:47 +0000

    Simplify List Item styling with custom theme

--- src/RecipeDialog.jsx
+++ src/RecipeDialog.jsx
@@ -1,6 +1,7 @@
 import { React, useState } from 'react';
 
-import { Typography, Box, TextField, ListItem, Dialog, Button, Container } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Typography, Box, TextField, ListItem, Dialog, Button, Container, Divider } from '@mui/material';
 
 import SelectAvatar from './SelectAvatar';
 import RecipePhoto from './RecipePhoto';
@@ -8,6 +9,20 @@ import SaveRecipe from './SaveRecipe';
 import FilterByTags from './FilterByTags';
 import AddMethod from './AddMethod';
 import AddIngredientsDialog from './AddIngredientsDialog'
+
+const RecipeDialogListItemTheme = createTheme({
+    components: {
+        MuiListItem: {
+            styleOverrides: {
+                root: {
+                    paddingRight: '0',
+                    paddingLeft: '0',
+                    },
+                            
+            },
+        },
+    },
+});
 
 /**
  * Recipe Dialog Component used for creating and editing recipies 
@@ -24,44 +39,46 @@ function RecipeDialog({ open, closeRecipeDialog }) {
     };
 
     return (
-        <Dialog fullWidth open={open} onClose={closeRecipeDialog} aria-labelledby="parent-dialog-title" aria-describedby="parent-dialog-description">
-            <Box p={4} display='flex' flexDirection='column' alignItems='start' gap='2'>
-                <Typography variant="h5" sx={{ marginBottom: '10px' }}>
-                    Add New Recipe
-                </Typography>
-                <ListItem sx={{ paddingRight: '0', paddingLeft: '0' }} divider>
-                    <TextField margin="dense" id="Recipe title" label="Add recipe title" type="text" fullWidth />
-                </ListItem>
-                <ListItem sx={{ paddingRight: '0', paddingLeft: '0' }} divider>
-                    <TextField margin="dense" id="Uploader name" label="Uploader (your name)" type="text" fullWidth />
-                </ListItem>
-                <ListItem sx={{ paddingRight: '0', paddingLeft: '0' }} divider>
-                    <SelectAvatar />
-                </ListItem>
-                <ListItem sx={{ paddingRight: '0', paddingLeft: '0' }} divider>
-                    <RecipePhoto />
-                </ListItem>
-                <ListItem sx={{ paddingRight: '0', paddingLeft: '0' }} divider>
-                    <TextField margin="dense" id="Recipe description" label="Add recipe description" type="text" fullWidth />
-                </ListItem>
-                <ListItem sx={{ paddingRight: '0', paddingLeft: '0' }} divider>
-                    <Container sx={{ width: '100%', display: 'flex', align: 'center', paddingTop: '5px', paddingBottom: '5px', justifyContent: 'space-between', paddingRight: '0', paddingLeft: '0' }}>
-                        <Typography color="textSecondary">Add ingredients</Typography>
-                        <Button variant="outlined" onClick={handleIngredientsDialogOpen}>
-                            Ingredients
-                        </Button>
-                        <AddIngredientsDialog open={openIngredientsDialog} closeIngredientsDialog={handleIngredientsDialogClose} />
-                    </Container>
-                </ListItem>
-                <ListItem sx={{ paddingRight: '0', paddingLeft: '0' }} divider>
-                    <AddMethod />
-                </ListItem>
-                <ListItem sx={{ paddingRight: '0', paddingLeft: '0' }} divider>
-                    <FilterByTags />
-                </ListItem>
-                <SaveRecipe closeRecipeDialog={closeRecipeDialog} />
-            </Box>
-        </Dialog>
+        <ThemeProvider theme={RecipeDialogListItemTheme}>
+            <Dialog fullWidth open={open} onClose={closeRecipeDialog} aria-labelledby="parent-dialog-title" aria-describedby="parent-dialog-description">
+                <Box p={4} display='flex' flexDirection='column' alignItems='start' gap='2'>
+                    <Typography variant="h5" sx={{ marginBottom: '10px' }}>
+                        Add New Recipe
+                    </Typography>
+                    <ListItem divider>
+                        <TextField margin="dense" id="Recipe title" label="Add recipe title" type="text" fullWidth />
+                    </ListItem>
+                    <ListItem sx={{ paddingRight: '0', paddingLeft: '0' }} divider>
+                        <TextField margin="dense" id="Uploader name" label="Uploader (your name)" type="text" fullWidth />
+                    </ListItem>
+                    <ListItem divider>
+                        <SelectAvatar />
+                    </ListItem>
+                    <ListItem divider>
+                        <RecipePhoto />
+                    </ListItem>
+                    <ListItem divider>
+                        <TextField margin="dense" id="Recipe description" label="Add recipe description" type="text" fullWidth />
+                    </ListItem>
+                    <ListItem divider>
+                        <Container sx={{ width: '100%', display: 'flex', align: 'center', paddingTop: '5px', paddingBottom: '5px', justifyContent: 'space-between', paddingRight: '0', paddingLeft: '0' }}>
+                            <Typography color="textSecondary">Add ingredients</Typography>
+                            <Button variant="outlined" onClick={handleIngredientsDialogOpen}>
+                                Ingredients
+                            </Button>
+                            <AddIngredientsDialog open={openIngredientsDialog} closeIngredientsDialog={handleIngredientsDialogClose} />
+                        </Container>
+                    </ListItem>
+                    <ListItem divider>
+                        <AddMethod />
+                    </ListItem>
+                    <ListItem divider>
+                        <FilterByTags />
+                    </ListItem>
+                    <SaveRecipe closeRecipeDialog={closeRecipeDialog} />
+                </Box>
+            </Dialog>
+        </ThemeProvider>
     )
 };
 
