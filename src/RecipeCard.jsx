0b7f9611eb8fb594349fb480cad4db8d158f1fa4
Dom Mistry <dominicmistry1@gmail.com>
2023-01-14 23:05:57 +0000

    add box shadow to recipe cards

--- src/RecipeCard.jsx
+++ src/RecipeCard.jsx
@@ -105,7 +105,7 @@ function RecipeCard() {
   };
 
   return (
-    <Card sx={{ height: "100%", display: "flex", flexDirection: "column" }}>
+    <Card sx={{ height: "100%", display: "flex", flexDirection: "column", boxShadow: "12" }}>
       <CardHeader
         avatar={
           <Avatar sx={{ bgcolor: green[500] }} aria-label="recipe">
@@ -155,10 +155,7 @@ function RecipeCard() {
         title="Image title"
       />
       <CardContent sx={{ flexGrow: 1 }}>
-        <Typography>
-          This is a recipe card. You can use this section to big up the recipe and tempt people to
-          click it.
-        </Typography>
+        <Typography>This is a recipe card. You can use this section to big up the recipe and tempt people to click it.</Typography>
       </CardContent>
       <CardActions disableSpacing>
         <IconButton aria-label="share">
@@ -172,40 +169,26 @@ function RecipeCard() {
           <ThumbDownIcon />
           {`${countDown === 0 ? " " : countDown}`}
         </Button>
-        <ExpandMore
-          expand={expanded ? 1 : 0}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
+        <ExpandMore expand={expanded ? 1 : 0} onClick={handleExpandClick} aria-expanded={expanded} aria-label="show more">
           <ExpandMoreIcon />
         </ExpandMore>
       </CardActions>
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
           <Typography paragraph>Method:</Typography>
+          <Typography paragraph>Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10 minutes.</Typography>
           <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-            minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-            heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-            browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-            and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-            pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high heat. Add chicken, shrimp and chorizo, and cook,
+            stirring occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken and chorizo
+            in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook, stirring often until thickened and fragrant,
+            about 10 minutes. Add saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
           </Typography>
           <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-            without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-            medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-            again without stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don&apos;t open.)
+            Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook without stirring, until most of the liquid is
+            absorbed, 15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook again
+            without stirring, until mussels have opened and rice is just tender, 5 to 7 minutes more. (Discard any mussels that don&apos;t open.)
           </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
-          </Typography>
+          <Typography>Set aside off of the heat to let rest for 10 minutes, and then serve.</Typography>
         </CardContent>
       </Collapse>
     </Card>
