107c98d761f18be3c2a05297eaf505944a38522b
Dom Mistry <dominicmistry1@gmail.com>
2023-01-20 20:45:19 +0000

    Add comments for the StyledMenu component

--- src/features/viewRecipeCard/components/StyledMenu.jsx
+++ src/features/viewRecipeCard/components/StyledMenu.jsx
@@ -1,9 +1,18 @@
 import { Menu } from "@mui/material";
-import PropTypes from "prop-types";
 import { React } from "react";
 
+/**
+ * Styled Menu is a custom Menu component with a number of custom theme attributes applied.
+ * This is to greatly enahnce the position of the Menu as well as it's look and feel.
+ *
+ * The PropTypes check has not been used with this component as the Spread Props (...props) function
+ * has been used. This allows for generic menu props to be passed into this component which will
+ * handle them.
+ */
 export default function StyledMenu(props) {
   return (
+    //These menu props specify where the anchor will be set in terms of both
+    //the origin component and on the popout menu component.
     <Menu
       elevation={0}
       anchorOrigin={{
@@ -18,7 +27,3 @@ export default function StyledMenu(props) {
     />
   );
 }
-
-StyledMenu.propTypes = {
-  props: PropTypes.any,
-};
