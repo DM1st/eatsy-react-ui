e79ac62be9d0a55a9e260d919a2ea1d78355360c
Dom Mistry <dominicmistry1@gmail.com>
2023-02-04 22:44:33 +0000

    fix typo in selectAvatar comment

--- src/features/selectAvatar/components/SelectAvatarDialog.jsx
+++ src/features/selectAvatar/components/SelectAvatarDialog.jsx
@@ -13,7 +13,7 @@ import { SelectAvatarDialogContext } from "../../addRecipe/contexts/SelectAvatar
  * The child dialog of the RecipeDialog for selecting the avatar of the uploader for the recipe.
  */
 export function SelectAvatarDialog() {
-  //Access the SelectAvatar state from the SelectAvatare context API
+  //Access the SelectAvatar state from the SelectAvatar context API
   const { openSelectAvatarDialog, changeSelectAvatarDialogOpenStatus } = useContext(SelectAvatarDialogContext);
 
   //Props for the SelectAvatar usage of the ListItemFAB component for picking a custom avatar icon.
