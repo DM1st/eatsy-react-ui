665955b743f8a05d8392ec35c297ed4a4acb19ac
Dom Mistry <dominicmistry1@gmail.com>
2023-02-08 23:00:38 +0000

    retrieve all recipes

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+/**
+ * Base instance of axios for the codebase.
+ * Defines base URL and other configuration elements.
+ */
 export default axios.create({
   baseURL: "http://localhost:8080/",
 });
--- src/components/RecipeCardGrid.jsx
+++ src/components/RecipeCardGrid.jsx
@@ -1,6 +1,7 @@
 import { Grid, Container } from "@mui/material";
-import { React } from "react";
-import { cards } from "@/assets/PlaceholderData";
+import axios from "axios";
+import { React, useState } from "react";
+import { useEffect } from "react";
 import { RecipeCard } from "@/features/viewRecipeCard";
 
 /**
@@ -8,13 +9,23 @@ import { RecipeCard } from "@/features/viewRecipeCard";
  * Wraps each recipeCard in a resonsive grid to adjust card size as the screen size is adjusted.
  */
 export default function RecipeCardGrid() {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/api/retrieveAllRecipes").then((res) => {
+      const response = res.data;
+      console.log(response); //TODO remove
+      setCards(Array.from(response));
+    });
+  }, []);
+
   return (
     <Container sx={{ padding: "20px 0" }}>
       {/* Flex Grid for containing all RecipeCard instances */}
       <Grid container spacing={4}>
         {cards.map((card) => (
           /**Flex item behaviour, RecipeCard is responsive to screen/browserWindow size */
-          <Grid item key={card} xs={12} sm={6} md={4}>
+          <Grid item key={card.key} xs={12} sm={6} md={4}>
             <RecipeCard />
           </Grid>
         ))}
