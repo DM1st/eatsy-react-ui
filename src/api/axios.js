aefb5f112831443701b45dd10dc258a18ca052cc
Dom Mistry <dominicmistry1@gmail.com>
2023-02-09 00:08:48 +0000

    added comment to explain cetralised axios instance

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,14 @@ import axios from "axios";
 
 /**
  * Base instance of axios for the codebase.
- * Defines base URL and other configuration elements.
+ * Defines base URL and other configuration elements in a centralised location.
+ *
+ * Allows for this exported object to be named as something concise (such as API) in the
+ * consuming code when it is imported. This allows for it to be used as a shorthand for the
+ * Axios instance e.g.
+ * API.get("api/retirveAllRecipes")
+ * rather than
+ * axios.get("http://localhost:8080/api/retrieveAllRecipes")
  */
 export default axios.create({
   baseURL: "http://localhost:8080/",
