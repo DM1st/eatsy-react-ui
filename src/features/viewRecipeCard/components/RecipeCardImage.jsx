32cbf5742b4fddc73f6081eeb888016d081617e7
Dom Mistry <dominicmistry1@gmail.com>
2023-04-06 22:11:01 +0100

    simplified the GET for recipeImages

--- src/features/viewRecipeCard/components/RecipeCardImage.jsx
+++ src/features/viewRecipeCard/components/RecipeCardImage.jsx
@@ -1,22 +1,16 @@
 import { CardMedia } from "@mui/material";
 import PropTypes from "prop-types";
 import { React } from "react";
-import { UseRecipeImageState } from "@/hooks/useRecipeImageState";
 
 /**
- * Recipe card image component to display the Recipe image on the Recipe Card in 16:9 aspect ratio
+ * Recipe card image component to display the Recipe image on the Recipe Card
  */
 export default function RecipeCardImage({ imageTitle }) {
-  //Access the state that retrieves and holds the recipes from the eatsy service.
-  const { recipeImage } = UseRecipeImageState();
-
-  const blob = new Blob([new Uint8Array(recipeImage)], { type: "image/jpeg" });
-
   return (
-    <CardMedia /* sx={{ paddingTop: "56.25%" }} 16:9 aspect ratio*/ title={imageTitle}>
+    <CardMedia title={imageTitle}>
       <img
-        src={URL.createObjectURL(blob)}
-        alt=""
+        src={"http://localhost:8080/api/get-image-with-media-type"}
+        alt={imageTitle}
         style={{
           objectFit: "contain",
           width: "100%",
